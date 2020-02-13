eca3b2f47153731aa47d9d9d850be67212057f55
dynamodens <dmbugua66@gmail.com>
2020-02-13 11:50:18 +0300

    initial commit

---
+++ functions.js
@@ -0,0 +1,73 @@
+/*function subtraction(num1,num2){
+    var ans= num1-num2;
+    return ans;
+}
+console.log(subtraction(5,3));
+ function total(num1,num2){
+     return num1-num2;
+
+ }
+ var subtraction=total(9,5);
+ console.log(subtraction);*/
+/*function mult(num1,num2){
+    return num1*num2;
+}
+var ans=mult(5,5);
+console.log(ans);
+
+
+function reminder(num1,num2){
+    return num1%num2;
+
+}
+var rem=reminder(19,3);
+console.log(rem);
+
+var gm =prompt("Your Name");
+function morn(num1,num2){
+    return num1+" "+num2;
+}
+var good=morn("Good","Morning");
+document.write(good+" "+gm);*/
+/*var name=prompt("Your Name");
+var age=prompt("What is your age");
+var food=prompt("Favorite food");
+
+function me(one){
+    return one;
+}
+var one=me("whose age is");
+ 
+function like(two){
+    return two;
+}
+var two= like("likes");
+
+document.write(name+" "+ one+" "+ age+" "+ two+" "+ food);*/
+/*var maxMass=parseInt(prompt("Input Max's Mass (kg)"));
+var maxHeight=parseInt(prompt("Input Max's Height(m)"));
+var johnMass=parseInt(prompt("Input John's Mass (kg)"));
+var johnHeight=parseInt(prompt("Input John's Height(m)"));
+
+var maxBMI=maxMass/(maxHeight*maxHeight);
+var johnBMI=johnMass/(johnHeight*johnHeight);
+console.log(johnBMI +" " +maxBMI);
+function BMI(one,two){
+    if(maxBMI > johnBMI){
+        alert("max's BMI is greater than john's");
+    }
+    else {
+        alert("johns bmi is greater than max");
+    }
+    return;
+}
+BMI(maxBMI,johnBMI);*/
+var input1=parseInt(prompt("Type a number"));
+var input2=parseInt(prompt("Type another number"));
+
+function total(one,two){
+    return input1+input2;
+
+}
+var add=total(input1,input2);
+alert(add);
